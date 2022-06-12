bedcfd514886891616459c263338470e40b00fb7
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-12 17:41:20 +0800

    并发编程

--- Goroutine/01-启动单个goroutine.go
+++ Goroutine/01-启动单个goroutine.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func hello() {
 	fmt.Println("hello world")
@@ -8,4 +11,5 @@ func hello() {
 func main() { //开启一个主goroutine去指向main函数
 	go hello() //开启了一个goroutine去指向hello这个函数
 	fmt.Println("hello main")
+	time.Sleep(time.Second)
 }
