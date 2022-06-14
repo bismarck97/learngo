129c95e5adc55247d2fb3f502478d4d9e9ee54a0
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-14 21:25:59 +0800

    字符串操作

--- String/02-字符串类型转换.go
+++ String/02-字符串类型转换.go
@@ -20,7 +20,7 @@ func main0201() {
 	str := strconv.FormatFloat(1.234, 'f', 2, 64)
 	fmt.Println(str)
 }
-func main() {
+func main0202() {
 	//将字符串转换成其他内容
 	//1.将字符串转成bool
 	//str := "123false123"
@@ -33,12 +33,29 @@ func main() {
 	//}
 
 	//2.将字符串转成整型 数据要符合整数的规则
-	str := "123"
+	//str := "123"
 	//参数为：数据 进制 位数
 	//value, _ := strconv.ParseInt(str, 10, 64)
 	//fmt.Println(value)
 	//直接按照10进制转换
-	value, _ := strconv.Atoi(str)
-	fmt.Println(value)
+	//value, _ := strconv.Atoi(str)
+	//fmt.Println(value)
 
+	//3.将字符串转成浮点型
+	str := "1.2345"
+	//参数为 数据 位数
+	value, _ := strconv.ParseFloat(str, 64)
+	fmt.Println(value)
+}
+func main() {
+	b := make([]byte, 0, 1024)
+	//将bool类型放在指定切片中
+	b = strconv.AppendBool(b, false)
+	//将整数类型放在指定切片中
+	b = strconv.AppendInt(b, 123, 10)
+	//将浮点类型放在指定切片中
+	b = strconv.AppendFloat(b, 3.1415, 'f', 3, 64)
+	//将字符串类型放在指定切片中
+	b = strconv.AppendQuote(b, "张三")
+	fmt.Println(string(b))
 }
